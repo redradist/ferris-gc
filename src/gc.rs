63ad6bf8a55e1669a2165125d54c7079edcca3a0
redradist <redradist@gmail.com>
2020-05-06 01:52:42 +0300

    Updated gc.rs

--- src/gc.rs
+++ src/gc.rs
@@ -2,7 +2,7 @@ pub mod sync;
 
 use std::cell::Cell;
 use std::collections::HashMap;
-use std::alloc::{alloc, Layout, dealloc};
+use std::alloc::{alloc, dealloc, Layout};
 
 pub trait Trace {
     fn is_root(&self) -> bool;
@@ -38,43 +38,45 @@ macro_rules! primitive_types {
 }
 
 primitive_types!(
-    u8, i8, u16, i16, u32, i32, u64, i64, u128, i128
+    u8, i8, u16, i16, u32, i32, u64, i64, u128, i128,
+    f32, f64,
+    bool
 );
 
 struct GcInfo {
-    is_root: Cell<bool>,
     has_root: Cell<bool>,
 }
 
-struct GcPtr<T> where T: 'static + Sized + Trace {
+pub struct GcPtr<T> where T: 'static + Sized + Trace {
     info: GcInfo,
-    t: Option<T>,
+    t: T,
+}
+
+impl<T> Deref for GcPtr<T> where T: 'static + Sized + Trace {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.t
+    }
 }
 
 impl<T> Trace for GcPtr<T> where T: Sized + Trace {
     fn is_root(&self) -> bool {
-        self.info.is_root.get()
+        unreachable!("is_root on GcPtr is unreachable !!");
     }
 
     fn reset_root(&self) {
-        self.info.is_root.set(false);
-        if let Some(t) = &self.t {
-            t.reset_root();
-        }
+        self.t.reset_root();
     }
 
     fn trace(&self) {
         self.info.has_root.set(true);
-        if let Some(t) = &self.t {
-            t.trace();
-        }
+        self.t.trace();
     }
 
     fn reset(&self) {
         self.info.has_root.set(false);
-        if let Some(t) = &self.t {
-            t.reset();
-        }
+        self.t.reset();
     }
 
     fn is_traceable(&self) -> bool {
@@ -82,26 +84,72 @@ impl<T> Trace for GcPtr<T> where T: Sized + Trace {
     }
 }
 
-pub struct Gc<T> where T: 'static + Sized + Trace {
+pub struct GcInternal<T> where T: 'static + Sized + Trace {
+    is_root: Cell<bool>,
     ptr: *const GcPtr<T>,
 }
 
-impl<T> Clone for Gc<T> where T: 'static + Sized + Trace {
-    fn clone(&self) -> Self {
-        let gc = Gc {
-            ptr: self.ptr
-        };
-        unsafe {
-            (*gc.ptr).info.is_root.set(true);
-        }
-        gc
+impl<T> Trace for GcInternal<T> where T: Sized + Trace {
+    fn is_root(&self) -> bool {
+        self.is_root.get()
     }
 
-    fn clone_from(&mut self, source: &Self) {
-        let is_root_prev = self.is_root();
-        self.ptr = source.ptr;
+    fn reset_root(&self) {
+        self.is_root.set(false);
+        if !self.ptr.is_null() {
+            unsafe {
+                (*self.ptr).reset_root();
+            }
+        }
+    }
+
+    fn trace(&self) {
+        if !self.ptr.is_null() {
+            unsafe {
+                (*self.ptr).trace();
+            }
+        }
+    }
+
+    fn reset(&self) {
+        if !self.ptr.is_null() {
+            unsafe {
+                (*self.ptr).reset();
+            }
+        }
+    }
+
+    fn is_traceable(&self) -> bool {
+        if !self.ptr.is_null() {
+            unsafe {
+                (*self.ptr).is_traceable()
+            }
+        } else {
+            true
+        }
+    }
+}
+
+impl<T> Deref for GcInternal<T> where T: 'static + Sized + Trace {
+    type Target = GcPtr<T>;
+
+    fn deref(&self) -> &Self::Target {
         unsafe {
-            (*self.ptr).info.is_root.set(is_root_prev);
+            &(*self.ptr)
+        }
+    }
+}
+
+pub struct Gc<T> where T: 'static + Sized + Trace {
+    internal_ptr: *mut GcInternal<T>,
+}
+
+impl<T> Deref for Gc<T> where T: 'static + Sized + Trace {
+    type Target = GcInternal<T>;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe {
+            &(*self.internal_ptr)
         }
     }
 }
@@ -112,57 +160,151 @@ impl<T> Gc<T> where T: Sized + Trace {
             gc.borrow_mut().create_gc(t)
         })
     }
+
+    pub fn null() -> Gc<T> {
+        LOCAL_GC.with(move |gc| unsafe {
+            gc.borrow_mut().null_gc()
+        })
+    }
+}
+
+impl<T> Clone for Gc<T> where T: 'static + Sized + Trace {
+    fn clone(&self) -> Self {
+        let gc = LOCAL_GC.with(move |gc| unsafe {
+            gc.borrow_mut().null_gc()
+        });
+        unsafe {
+            (*gc.internal_ptr).is_root.set(true);
+        }
+        gc
+    }
+
+    fn clone_from(&mut self, source: &Self) {
+        let tracer = self.internal_ptr;
+        LOCAL_GC.with(move |gc| {
+            gc.borrow_mut().unregister_root(tracer);
+        });
+        unsafe {
+            (*self.internal_ptr).ptr = (*source.internal_ptr).ptr;
+        }
+    }
 }
 
 impl<T> Drop for Gc<T> where T: Sized + Trace {
     fn drop(&mut self) {
-        self.reset_root();
+        LOCAL_GC.with(move |gc| {
+            gc.borrow_mut().unregister_root(self.internal_ptr);
+        });
     }
 }
 
 impl<T> Trace for Gc<T> where T: Sized + Trace {
     fn is_root(&self) -> bool {
         unsafe {
-            (*self.ptr).is_root()
+            (*self.internal_ptr).is_root()
         }
     }
 
     fn reset_root(&self) {
         unsafe {
-            (*self.ptr).reset_root();
+            (*self.internal_ptr).reset_root();
         }
     }
 
     fn trace(&self) {
         unsafe {
-            (*self.ptr).trace();
+            (*self.internal_ptr).trace();
         }
     }
 
     fn reset(&self) {
         unsafe {
-            (*self.ptr).reset();
+            (*self.internal_ptr).reset();
         }
     }
 
     fn is_traceable(&self) -> bool {
         unsafe {
-            (*self.ptr).is_traceable()
+            (*self.internal_ptr).is_traceable()
+        }
+    }
+}
+
+pub struct GcCellInternal<T> where T: 'static + Sized + Trace {
+    is_root: Cell<bool>,
+    ptr: *const RefCell<GcPtr<T>>,
+}
+
+impl<T> Trace for GcCellInternal<T> where T: Sized + Trace {
+    fn is_root(&self) -> bool {
+        self.is_root.get()
+    }
+
+    fn reset_root(&self) {
+        self.is_root.set(false);
+        if !self.ptr.is_null() {
+            unsafe {
+                (*self.ptr).borrow().reset_root();
+            }
+        }
+    }
+
+    fn trace(&self) {
+        if !self.ptr.is_null() {
+            unsafe {
+                (*self.ptr).borrow().trace();
+            }
+        }
+    }
+
+    fn reset(&self) {
+        if !self.ptr.is_null() {
+            unsafe {
+                (*self.ptr).borrow().reset();
+            }
+        }
+    }
+
+    fn is_traceable(&self) -> bool {
+        if !self.ptr.is_null() {
+            unsafe {
+                (*self.ptr).borrow().is_traceable()
+            }
+        } else {
+            true
+        }
+    }
+}
+
+impl<T> Deref for GcCellInternal<T> where T: 'static + Sized + Trace {
+    type Target = RefCell<GcPtr<T>>;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe {
+            &(*self.ptr)
         }
     }
 }
 
 pub struct GcCell<T> where T: 'static + Sized + Trace {
-    ptr: *const RefCell<GcPtr<T>>,
+    internal_ptr: *mut GcCellInternal<T>,
 }
 
-impl<T> Clone for GcCell<T> where T: 'static + Sized + Trace {
-    fn clone(&self) -> Self {
-        unimplemented!()
+impl<T> Drop for GcCell<T> where T: Sized + Trace {
+    fn drop(&mut self) {
+        LOCAL_GC.with(move |gc| {
+            gc.borrow_mut().unregister_root(self.internal_ptr);
+        });
     }
+}
 
-    fn clone_from(&mut self, source: &Self) {
+impl<T> Deref for GcCell<T> where T: 'static + Sized + Trace {
+    type Target = GcCellInternal<T>;
 
+    fn deref(&self) -> &Self::Target {
+        unsafe {
+            &(*self.internal_ptr)
+        }
     }
 }
 
@@ -171,6 +313,32 @@ impl<T> GcCell<T> where T: Sized + Trace {
         LOCAL_GC.with(move |gc| unsafe {
             gc.borrow_mut().create_gc_cell(t)
         })
+    }
+
+    pub fn null() -> GcCell<T> {
+        LOCAL_GC.with(move |gc| unsafe {
+            gc.borrow_mut().null_gc_cell()
+        })
+    }
+}
+
+impl<T> Clone for GcCell<T> where T: 'static + Sized + Trace {
+    fn clone(&self) -> Self {
+        let gc = LOCAL_GC.with(move |gc| unsafe {
+            gc.borrow_mut().null_gc_cell()
+        });
+        gc.is_root.set(true);
+        gc
+    }
+
+    fn clone_from(&mut self, source: &Self) {
+        let tracer = self.internal_ptr;
+        LOCAL_GC.with(move |gc| {
+            gc.borrow_mut().unregister_root(tracer);
+        });
+        unsafe {
+            (*self.internal_ptr).ptr = (*source.internal_ptr).ptr;
+        }
     }
 }
 
@@ -182,6 +350,7 @@ impl<T> Trace for GcCell<T> where T: Sized + Trace {
     }
 
     fn reset_root(&self) {
+        self.is_root.set(false);
         unsafe {
             (*self.ptr).borrow().reset_root();
         }
@@ -206,61 +375,106 @@ impl<T> Trace for GcCell<T> where T: Sized + Trace {
     }
 }
 
+type GcObjMem = *mut u8;
+
 pub struct GarbageCollector {
-    vec: HashMap<*const dyn Trace, (*mut u8, Layout)>
+    vec: HashMap<*const dyn Trace, ((GcObjMem, Layout), (GcObjMem, Layout))>,
 }
 
 impl GarbageCollector {
     fn new() -> GarbageCollector {
-        GarbageCollector { vec: HashMap::new() }
-    }
-
-    pub fn strategy() {
-
-    }
-
-    unsafe fn create_gc<T>(&mut self, t: T) -> Gc<T> where T: Sized + Trace {
-        let gc = Gc {
-            ptr: self.get_gc_ptr(t),
-        };
-        gc.reset_root();
-        unsafe {
-            (*gc.ptr).info.is_root.set(true);
+        GarbageCollector {
+            vec: HashMap::new(),
         }
+    }
+
+    pub fn strategy(&mut self) {
+        // std::thread::spawn(move || unsafe {
+        //     self.collect();
+        // });
+    }
+
+    unsafe fn create_gc<T>(&mut self, t: T) -> Gc<T>
+        where T: Sized + Trace {
+        let (gc_inter_ptr, mem_info_internal_ptr) = self.get_gc_inter_ptr::<T>();
+        let (gc_ptr, mem_info_gc_ptr) = self.get_gc_ptr::<T>();
+        let gc = Gc {
+            internal_ptr: gc_inter_ptr,
+        };
+        (*gc_ptr).info.has_root.set(false);
+        (*gc_ptr).t = t;
+        (*gc.internal_ptr).ptr = gc_ptr;
+        gc.reset_root();
+        (*gc.internal_ptr).is_root.set(true);
+        self.register_root(gc.internal_ptr, (mem_info_internal_ptr, mem_info_gc_ptr));
         gc
     }
 
-    unsafe fn create_gc_cell<T>(&mut self, t: T) -> GcCell<T> where T: Sized + Trace {
-        let gc_cell = GcCell {
-            ptr: self.get_ref_cell_gc_ptr(t),
-        };
-        gc_cell.reset_root();
-        unsafe {
-            (*gc_cell.ptr).borrow().info.is_root.set(true);
+    unsafe fn null_gc<T>(&mut self) -> Gc<T> where T: Sized + Trace {
+        let (gc_inter_ptr, mem_info) = self.get_gc_inter_ptr::<T>();
+        (*gc_inter_ptr).ptr = std::ptr::null();
+        Gc {
+            internal_ptr: gc_inter_ptr,
         }
-        gc_cell
     }
 
-    unsafe fn get_gc_ptr<T>(&mut self, t: T) -> *mut GcPtr<T> where T: Sized + Trace {
+    unsafe fn create_gc_cell<T>(&mut self, t: T) -> GcCell<T> where T: Sized + Trace {
+        let (gc_cell_inter_ptr, mem_info_internal_ptr) = self.get_gc_cell_inter_ptr::<T>();
+        let (gc_ptr, mem_info_gc_ptr) = self.get_ref_cell_gc_ptr::<T>();
+        let gc = GcCell {
+            internal_ptr: gc_cell_inter_ptr,
+        };
+        (*gc_ptr).borrow().info.has_root.set(false);
+        (*gc_ptr).borrow_mut().t = t;
+        (*gc.internal_ptr).ptr = gc_ptr;
+        gc.reset_root();
+        (*gc.internal_ptr).is_root.set(true);
+        self.register_root(gc.internal_ptr, (mem_info_internal_ptr, mem_info_gc_ptr));
+        gc
+    }
+
+    unsafe fn null_gc_cell<T>(&mut self) -> GcCell<T> where T: Sized + Trace {
+        let (gc_inter_ptr, mem_info) = self.get_gc_cell_inter_ptr::<T>();
+        (*gc_inter_ptr).ptr = std::ptr::null();
+        GcCell {
+            internal_ptr: gc_inter_ptr,
+        }
+    }
+
+    fn register_root(&mut self, root_ptr: *const dyn Trace, mem: ((GcObjMem, Layout), (GcObjMem, Layout))) {
+        self.vec.insert(root_ptr, mem);
+    }
+
+    fn unregister_root(&mut self, root_ptr: *const dyn Trace) {
+        self.vec.remove(&root_ptr);
+    }
+
+    unsafe fn get_gc_inter_ptr<T>(&mut self) -> (*mut GcInternal<T>, (GcObjMem, Layout)) where T: Sized + Trace {
+        let layout = Layout::new::<GcInternal<T>>();
+        let mem = alloc(layout);
+        let gc_inter_ptr: *mut GcInternal<T> = std::ptr::read(mem as *mut _);
+        (gc_inter_ptr, (mem, layout))
+    }
+
+    unsafe fn get_gc_ptr<T>(&mut self) -> (*mut GcPtr<T>, (GcObjMem, Layout)) where T: Sized + Trace {
         let layout = Layout::new::<GcPtr<T>>();
         let mem = alloc(layout);
-        let gc_ptr: *mut GcPtr<T> = std::ptr::read(mem as *const _);
-        (*gc_ptr).info.is_root.set(true);
-        (*gc_ptr).info.has_root.set(false);
-        (*gc_ptr).t = Some(t);
-        self.vec.insert(gc_ptr, (mem, layout));
-        gc_ptr
+        let gc_ptr: *mut GcPtr<T> = std::ptr::read(mem as *mut _);
+        (gc_ptr, (mem, layout))
     }
 
-    unsafe fn get_ref_cell_gc_ptr<T>(&mut self, t: T) -> *const RefCell<GcPtr<T>> where T: Sized + Trace {
+    unsafe fn get_gc_cell_inter_ptr<T>(&mut self) -> (*mut GcCellInternal<T>, (GcObjMem, Layout)) where T: Sized + Trace {
+        let layout = Layout::new::<GcInternal<T>>();
+        let mem = alloc(layout);
+        let gc_cell_inter_ptr: *mut GcCellInternal<T> = std::ptr::read(mem as *mut _);
+        (gc_cell_inter_ptr, (mem, layout))
+    }
+
+    unsafe fn get_ref_cell_gc_ptr<T>(&mut self) -> (*const RefCell<GcPtr<T>>, (*mut u8, Layout)) where T: Sized + Trace {
         let layout = Layout::new::<RefCell<GcPtr<T>>>();
         let mem = alloc(layout);
         let gc_ptr: *const RefCell<GcPtr<T>> = std::ptr::read(mem as *const _);
-        (*gc_ptr).borrow_mut().info.is_root.set(true);
-        (*gc_ptr).borrow_mut().info.has_root.set(false);
-        (*gc_ptr).borrow_mut().t = Some(t);
-        self.vec.insert((*gc_ptr).as_ptr(), (mem, layout));
-        gc_ptr
+        (gc_ptr, (mem, layout))
     }
 
     unsafe fn collect(&mut self) {
@@ -281,15 +495,82 @@ impl GarbageCollector {
         }
         for col in collected_objects {
             let del = self.vec[&col];
-            dealloc(del.0, del.1);
+            dealloc((del.0).0, (del.0).1);
+            dealloc((del.1).0, (del.1).1);
             self.vec.remove(&col);
         }
     }
 }
 
+// trait Strategy {
+//     fn start(&mut self);
+//     fn stop(&mut self);
+// }
+//
+// struct BasicStrategy<F>
+//     where F: Fn(),
+//           F: Send + 'static {
+//     is_active: AtomicBool,
+//     task: Arc<F>,
+//     task_join: Option<JoinHandle<()>>,
+// }
+//
+// impl<F> BasicStrategy<F>
+//     where F: Fn(),
+//           F: Send + 'static {
+//     fn new(f: Arc<F>) -> BasicStrategy<F> {
+//         BasicStrategy {
+//             is_active: AtomicBool::new(true),
+//             task: f,
+//             task_join: None,
+//         }
+//     }
+// }
+//
+// impl<F> Strategy for BasicStrategy<F>
+//     where F: Fn(),
+//           F: Send + 'static {
+//     fn start(&mut self) {
+//         let handler = self.task.clone();
+//         self.is_active.store(true, Ordering::Release);
+//         self.task_join = Some(std::thread::spawn(move || {
+//             (*handler)();
+//         }));
+//     }
+//
+//     fn stop(&mut self) {
+//         self.is_active.store(false, Ordering::Release);
+//         if let Some(task_join) = self.task_join {
+//             task_join.join();
+//         }
+//     }
+// }
+//
+// impl<F> Drop for BasicStrategy<F>
+//     where F: Fn(),
+//           F: Send + 'static {
+//     fn drop(&mut self) {
+//         self.stop();
+//     }
+// }
+
 use std::cell::RefCell;
+use std::ops::Deref;
+use std::thread;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::thread::JoinHandle;
+use std::rc::Rc;
+use std::sync::Arc;
+use std::borrow::BorrowMut;
 thread_local! {
     pub static LOCAL_GC: RefCell<GarbageCollector> = RefCell::new(GarbageCollector::new());
+    // static STRATEGY: Cell<BasicStrategy<Arc<&'static dyn Fn()>>> = {
+    //     LOCAL_GC.with(move |gc| {
+    //         Cell::new(BasicStrategy::new(Arc::new(move || {
+    //
+    //         })))
+    //     })
+    // };
 }
 
 #[cfg(test)]
@@ -320,7 +601,7 @@ mod tests {
     fn two_objects() {
         let mut one = Gc::new(1);
         one = Gc::new(2);
-        LOCAL_GC.with(move |gc| unsafe {
+        LOCAL_GC.with(move |gc| {
             assert_eq!(gc.borrow_mut().vec.len(), 2);
         });
     }
